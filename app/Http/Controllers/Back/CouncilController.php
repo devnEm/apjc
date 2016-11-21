9128d6c48da520698016b8cfbb68bd6304b8354e
devnem <nempointcom@hotmail.com>
2016-11-22 00:34:36 +0100

    Prepare for the council

---
+++ app/Http/Controllers/Back/CouncilController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Back;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Validator;
+use App\Http\Requests;
+
+use App\Models\Council;
+
+class CouncilController extends Controller
+{
+    //
+}
