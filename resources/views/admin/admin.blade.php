a37b008dc1f23206dbe4b6363894e290065b330f
devnem <nempointcom@hotmail.com>
2016-12-03 20:46:49 +0100

    new and less ugly admin page

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -1,35 +1,53 @@
 @extends('layouts.admin.app')
 @section('content')
 <div class="row">
-    <div class="col-md-3">
-        <div class="panel panel-info">
-            <div class="panel-heading">Inscrits</div>
-            <div class="panel-body">
-                {{count($users)}}
-            </div>
+  <div class="col-md-6">
+    <div class="panel panel-info" style="height:200px">
+        <div class="panel-heading">Adhérents</div>
+        <div class="panel-body">
+          <div class="row">
+            <label>Nbr d'inscrit : {{count($users)}}</label>
+          </div>
+          <div class="row">
+            <button type="button" class="btn btn-primary btn-sm">Ajouter un adhérent</button>
+            <button type="button" class="btn btn-success btn-sm">Gérer les adhérent</button>
+            <button type="button" class="btn btn-warning btn-sm">Envoyer des mails</button>
+          </div>
         </div>
     </div>
-    <div class="col-md-3">
-        <div class="panel panel-info">
-            <div class="panel-heading">Dashboard</div>
+  </div>
+    <div class="col-md-6">
+        <div class="panel panel-info" style="height:200px">
+            <div class="panel-heading">Bureau</div>
             <div class="panel-body">
-                You are in admin!
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div class="panel panel-info">
-            <div class="panel-heading">Dashboard</div>
-            <div class="panel-body">
-                You are in admin!
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3">
-        <div class="panel panel-info">
-            <div class="panel-heading">Dashboard</div>
-            <div class="panel-body">
-                You are in admin!
+              <div class="row">
+                <label>Nbr d'inscrit : {{count($users)}}</label>
+              </div>
+              <div class="row">
+                <button type="button" class="btn btn-primary">Ajouter un membre</button>
+                <button type="button" class="btn btn-success">Modifier les rôles</button>
+              </div>
+              <!-- <hr>
+                <div class="table table-responsive">
+                  <table class="table">
+                    <thead>
+                      <td>id</td>
+                      <td>nom</td>
+                      <td>email</td>
+                      <td>admin</td>
+                    </thead>
+                    <tbody>
+                      @foreach($users as $user)
+                      <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>@if($user->admin) true @else false @endif</td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div> -->
             </div>
         </div>
     </div>
@@ -37,27 +55,12 @@
 <div class="row">
   <div class="col-md-12">
     <div class="panel panel-info">
+      <div class="panel-heading">
+        Admin-site ZONE
+      </div>
       <div class="panel-body">
-        <div class="table table-responsive">
-          <table class="table">
-            <thead>
-              <td>id</td>
-              <td>nom</td>
-              <td>email</td>
-              <td>admin</td>
-            </thead>
-            <tbody>
-              @foreach($users as $user)
-              <tr>
-                <td>{{$user->id}}</td>
-                <td>{{$user->name}}</td>
-                <td>{{$user->email}}</td>
-                <td>@if($user->admin) true @else false @endif</td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div>
+        Admin-site ZONE
+        Acces à la gestion des messages d'informations du sites
       </div>
     </div>
   </div>
